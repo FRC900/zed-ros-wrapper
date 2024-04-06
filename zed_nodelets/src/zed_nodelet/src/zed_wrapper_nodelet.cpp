d5ee4cf3f701109b2638fef716e32cb4f3db03c2
Kevin Jaget <kjaget@nc.rr.com>
2024-04-06 17:16:21 -0400

    Enable 15FPS setting for SVGA

--- zed_nodelets/src/zed_nodelet/src/zed_wrapper_nodelet.cpp
+++ zed_nodelets/src/zed_nodelet/src/zed_wrapper_nodelet.cpp
@@ -1570,7 +1570,7 @@ void ZEDWrapperNodelet::checkResolFps()
       break;
 
     case sl::RESOLUTION::HD1200:
-      if (mCamFrameRate == 60 || mCamFrameRate == 30)
+      if (mCamFrameRate == 60 || mCamFrameRate == 30 || mCamFrameRate == 15)
       {
         break;
       }
